2250317cf8174a1e226ea18ec9399c2bc3beeee0
Vinicius <viniciuscostadealmeida98@gmail.com>
2024-10-10 14:59:11 -0300

    create authenticate use-case

---
+++ src/use-cases/users/authenticate.ts
@@ -0,0 +1,36 @@
+import { User } from "@prisma/client"
+import { UsersRepository } from "../../repositories/users-repository"
+import { ResourceNotFoundError } from "../../errors/resource-not-found-error"
+import { compare } from "bcryptjs"
+import { CredentialsInvalidError } from "../../errors/credentials-invalid-error"
+
+interface AuthenticateRequest{
+    email: string
+    password: string
+}
+
+interface AuthenticateResponse{
+    user: User
+}
+
+export class AuthenticateuseCase{
+    constructor(private usersRepository: UsersRepository){}
+
+    async execute({ email, password } : AuthenticateRequest) : Promise<AuthenticateResponse>{
+        const user = await this.usersRepository.findByEmail(email)
+
+        if(!user){
+            throw new ResourceNotFoundError()
+        }
+
+        const doesPasswordMatch = await compare(password, user.password)
+
+        if(!doesPasswordMatch){
+            throw new CredentialsInvalidError()
+        }
+
+        return {
+            user
+        }
+    }
+}

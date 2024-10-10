1769bb485d90bc1addee68ba7d6e7752a4f90ae9
Vinicius <viniciuscostadealmeida98@gmail.com>
2024-10-10 16:19:19 -0300

    test get-user-profile-use-case

---
+++ src/use-cases/users/get-user-profile.spec.ts
@@ -0,0 +1,27 @@
+import { beforeEach, describe, expect, it } from "vitest";
+import { InMemmoryUsersRepository } from "../../repositories/in-memmory/in-memmory-users-repository";
+import { GetUserProfile } from "./get-list-user-profile";
+import { hash } from "bcryptjs";
+
+let usersRepository: InMemmoryUsersRepository
+let sut: GetUserProfile
+
+describe("Get user profile test", () => {
+    beforeEach(() => {
+        usersRepository = new InMemmoryUsersRepository()
+        sut = new GetUserProfile(usersRepository)
+    })
+
+    it('should be able to get user profile', async () => {
+        const createdUser = await usersRepository.create({
+            email: 'johndoe@example.com',
+            password: await hash("12345678", 6)
+        })
+
+        const { user } = await sut.execute({
+            userId: createdUser.id
+        })
+
+        expect(user.id).toEqual(expect.any(String))
+    })
+})
